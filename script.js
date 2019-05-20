9cb54197492c4e72de3a5d3b22b6ddef273caa84
mohamedkane <43444237+mohamedkane@users.noreply.github.com>
2019-05-20 09:14:16 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+    $("h1").click(function() { 
+    $(".light").toggle();
+});
+
+    $(".snack").click(function() { 
+    $(".sun").fadeToggle();
+});
+
+
+    $(".pack").click(function() { 
+    $(".train").fadeToggle();
+});
+
+$("#alp").click(function() {
+  $("alp").css("background", "green,yellow");
+    });
